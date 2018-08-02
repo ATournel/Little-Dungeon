9732b380c64d72f80354e99dffc5547c0dff0b80
ATournel <a.tournel@hotmail.fr>
2018-08-02 12:51:06 +0200

    Title and intro texts

--- Little Dungeon/src/Game.java
+++ Little Dungeon/src/Game.java
@@ -1,10 +1,17 @@
+import java.util.*;
 
 public class Game {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		BigTexts text1 = new BigTexts();
+		Scanner scan = new Scanner(System.in);
+		Scanner scan2 = new Scanner(System.in);
 		
 		
-
+		text1.printTitle();
+		text1.printIntro();
+		
+		
 	}
 
 }
