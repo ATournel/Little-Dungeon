485f826f5df4dc717dc466d872d84fb19d769802
ATournel <a.tournel@hotmail.fr>
2018-08-02 11:36:11 +0200

    Class Creation

---
+++ Little Dungeon/src/Character.java
@@ -0,0 +1,19 @@
+
+
+public class Character {
+
+			String name;
+			int strength;
+			int dext;
+			int intell;
+			int life;
+			int mana;
+			int armor;
+			int magicArmor;
+		
+		/*System.out.println(Wise_Mage.getName()+", here are your stats:\nStrength(how many items you can carry and how hard you strike with melee!): "+Wise_Mage.getStrength()+"\n"
+		*		+"Dexterity(your evade stat and how hard you strike with ranged!): "+Wise_Mage.getDext()+"\nIntelligence(number and quality of skills you can lurn, also helps in some conversations): "+Wise_Mage.getIntell()+
+		*		"\nHealth points(how much damage you can take before facing your death): "+Wise_Mage.getLife()+"\nMana(how many magic points you can use to cast magics!): "+Wise_Mage.getMana());
+		*/
+}
+
