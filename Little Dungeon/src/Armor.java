9838e4f5c40b78f08180b80aaf0f97d6d3cc04f1
ATournel <a.tournel@hotmail.fr>
2018-08-02 13:37:12 +0200

    Added some items classes

---
+++ Little Dungeon/src/Armor.java
@@ -0,0 +1,26 @@
+
+public class Armor {
+	
+	String name;
+	int armor;
+	int magicArmor;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getArmor() {
+		return armor;
+	}
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+	public int getMagicArmor() {
+		return magicArmor;
+	}
+	public void setMagicArmor(int magicArmor) {
+		this.magicArmor = magicArmor;
+	}
+
+}
---
+++ Little Dungeon/src/Weapon.java
@@ -0,0 +1,26 @@
+
+public class Weapon {
+
+	String name;
+	int damage;
+	int magicDamage;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getMagicDamage() {
+		return magicDamage;
+	}
+	public void setMagicDamage(int magicDamage) {
+		this.magicDamage = magicDamage;
+	}
+	
+}
